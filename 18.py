5d12f0aa676c452bb6a73732a42e20d34548a7b2
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-08-16 21:24:10 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -6,13 +6,13 @@ b=float(input("Enter marks of Biology: "))
 
 avg=(p+c+b)/3
 
-if(avg>80):
+if(avg>=80):
     print(avg," Distinction")
-elif(avg>60):
+elif(avg>=60):
     print(avg," First Division")
-elif(avg>45):
+elif(avg>=45):
     print(avg," Second Division")
-elif(avg>40):
+elif(avg>=40):
     print(avg," Pass")
 else:
     print(avg," Promotion not granted")
